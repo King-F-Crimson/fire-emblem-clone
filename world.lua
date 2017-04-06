a4e7bfd450cef85e221600a88a9dd1a66ae9135f
Pieter Douglas <pd.pieterdouglas@gmail.com>
2017-04-06 22:28:39 +1200

    Put unit in unit_layer on map. Currently unit_layer is not drawn in map:draw(), using workaround for this time.

--- world.lua
+++ world.lua
@@ -11,16 +11,29 @@ function world.create()
     setmetatable(self, {__index = world})
 
     self.map = sti("maps/sample_map.lua")
-    self.unit = unit.create(0, 0)
+
+    unit_layer = self.map:addCustomLayer("unit_layer", 69)
+    unit_layer.units = {}
+    unit_layer.units.player = unit.create(-16, 0)
+
+    function unit_layer:update(dt)
+
+    end
+
+    function unit_layer:draw()
+        for _, unit in pairs(self.units) do
+            unit:draw()
+        end
+    end
 
     return self
 end
 
 function world:update()
-
+    self.map:update()
 end
 
 function world:draw()
     self.map:draw()
-    self.unit:draw()
+    self.map.layers.unit_layer:draw()
 end
