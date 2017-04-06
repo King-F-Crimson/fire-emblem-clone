0eeea9a5d50619fa6b90b34a06bd3a88fda0e828
Pieter Douglas <pd.pieterdouglas@gmail.com>
2017-04-06 13:59:22 +1200

    Make game fullscreen, add escape to close window, set scale (zoom) to 2.

--- main.lua
+++ main.lua
@@ -4,14 +4,48 @@ require("world")
 app = {}
 
 function love.load()
+    love.window.setMode(0, 0, {fullscreen = true})
+
+    app.camera = { x = 0, y = 0 }
     app.world = world.create()
 end
 
 function love.update()
+    local move = { x = 0, y = 0 }
+    if love.keyboard.isDown("up") then
+        move.y =  1
+    end
+    if love.keyboard.isDown("down") then
+        move.y =  -1
+    end
+    if love.keyboard.isDown("left") then
+        move.x =  1
+    end
+    if love.keyboard.isDown("right") then
+        move.x =  -1
+    end
+    if love.keyboard.isDown("lshift") then
+        move.y = move.y * 5
+        move.x = move.x * 5
+    end
+
+    app.camera.x = app.camera.x + move.x
+    app.camera.y = app.camera.y + move.y
+
     app.world:update()
 end
 
 function love.draw()
+    love.graphics.push()
+    love.graphics.scale(2   )
+    love.graphics.translate(app.camera.x, app.camera.y)
+
     app.world:draw()
+    love.graphics.pop()
 end
 
+function love.keypressed(key)
+    if key == "escape" then
+        love.window.close()
+    end
+end
