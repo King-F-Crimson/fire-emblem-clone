e23450bdd31fff8e295696ea40d26a766dbc118a
Pieter Douglas <pd.pieterdouglas@gmail.com>
2017-05-08 22:41:23 +1200

    Combat info now displays "-" when target_unit cannot counter attack.

--- combat_info.lua
+++ combat_info.lua
@@ -14,16 +14,27 @@ function combat_info.create(observer, ui, x, y)
 end
 
 function combat_info:set_combat_info(action)
+    local world = self.ui.world
+    local selected_unit = self.ui.selected_unit
+
     if action.action == "attack" then
-        local target_unit = self.ui.world:get_unit(action.data.tile_x, action.data.tile_y)
+        local target_unit = world:get_unit(action.data.tile_x, action.data.tile_y)
         if target_unit then
-            self.info = string.format(
-                "Attack power: %i\nHit rate: %i%%\n\nAttack power: %i\nHit rate: %i%%",
-                combat.get_attack_power(self.ui.world, self.ui.selected_unit, action.data.weapon, target_unit),
-                combat.get_hit_rate(self.ui.world, self.ui.selected_unit, action.data.weapon, target_unit),
-                combat.get_attack_power(self.ui.world, target_unit, target_unit.data.active_weapon, self.ui.selected_unit),
-                combat.get_hit_rate(self.ui.world, target_unit, target_unit.data.active_weapon, self.ui.selected_unit)
-            )
+            if target_unit:can_counter_attack(world, self.ui.plan_tile_x, self.ui.plan_tile_y) then
+                self.info = string.format(
+                    "Attack power: %i\nHit rate: %i%%\n\nAttack power: %i\nHit rate: %i%%",
+                    combat.get_attack_power(world, selected_unit, action.data.weapon, target_unit),
+                    combat.get_hit_rate(world, selected_unit, action.data.weapon, target_unit),
+                    combat.get_attack_power(world, target_unit, target_unit.data.active_weapon, selected_unit),
+                    combat.get_hit_rate(world, target_unit, target_unit.data.active_weapon, selected_unit)
+                )
+            else
+                self.info = string.format(
+                    "Attack power: %i\nHit rate: %i%%\n\nAttack power: -\nHit rate: -",
+                    combat.get_attack_power(world, selected_unit, action.data.weapon, target_unit),
+                    combat.get_hit_rate(world, selected_unit, action.data.weapon, target_unit)
+                )
+            end
         end
     end
 end
