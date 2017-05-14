68d06452b74c3c76c1ccbf39a1884294bc64daac
Pieter Douglas <pd.pieterdouglas@gmail.com>
2017-05-14 20:35:59 +1200

    Add instruction "Press any button to start!" to the title screen.

--- title_screen.lua
+++ title_screen.lua
@@ -7,6 +7,9 @@ function title_screen.create(application)
     self.title = love.graphics.newText(self.application.font, "Fire Emblem Clone")
     self.title_width = self.title:getWidth()
 
+    self.instruction = love.graphics.newText(self.application.font, "Press any button to start!")
+    self.instruction_width = self.instruction:getWidth()
+
     self.zoom = 4
 
     return self
@@ -26,5 +29,6 @@ function title_screen:draw()
     love.graphics.push()
         love.graphics.scale(self.zoom)
         love.graphics.draw(self.title, (love.graphics.getWidth() / self.zoom - self.title_width) / 2, 20)
+        love.graphics.draw(self.instruction, (love.graphics.getWidth() / self.zoom - self.instruction_width) / 2, 200)
     love.graphics.pop()
 end
