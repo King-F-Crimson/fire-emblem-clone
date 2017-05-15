bc4d4c16c93b43f8c9a92427b83ac13a6f2d24f5
Pieter Douglas <pd.pieterdouglas@gmail.com>
2017-05-15 22:44:28 +1200

    Make title texts align with screen height.

--- title_screen.lua
+++ title_screen.lua
@@ -6,9 +6,11 @@ function title_screen.create(application)
 
     self.title = love.graphics.newText(self.application.font, "Fire Emblem Clone")
     self.title_width = self.title:getWidth()
+    self.title_height = self.title:getHeight()
 
     self.instruction = love.graphics.newText(self.application.font, "Press any button to start!")
     self.instruction_width = self.instruction:getWidth()
+    self.instruction_height = self.instruction:getHeight()
 
     self.zoom = 4
 
@@ -28,7 +30,7 @@ end
 function title_screen:draw()
     love.graphics.push()
         love.graphics.scale(self.zoom)
-        love.graphics.draw(self.title, (love.graphics.getWidth() / self.zoom - self.title_width) / 2, 20)
-        love.graphics.draw(self.instruction, (love.graphics.getWidth() / self.zoom - self.instruction_width) / 2, 200)
+        love.graphics.draw(self.title, (love.graphics.getWidth() / self.zoom - self.title_width) / 2, self.title_height)
+        love.graphics.draw(self.instruction, (love.graphics.getWidth() / self.zoom - self.instruction_width) / 2, love.graphics.getHeight() / self.zoom - self.instruction_height * 2)
     love.graphics.pop()
 end
