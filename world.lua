23ccff99c14fbf65a2a3a549abe463a92912b651
Pieter Douglas <pd.pieterdouglas@gmail.com>
2017-06-29 21:07:54 +1200

    Restore enemy units.

--- world.lua
+++ world.lua
@@ -32,10 +32,10 @@ function world.create(observer, teams, animation)
 
     -- Create enemy units.
     unit_layer:create_unit(unit_class.sword_fighter, 3, 14, { active_weapon = 1, weapons = {weapon_class.iron_sword}, team = self.teams[2] })
-    -- unit_layer:create_unit(unit_class.sword_fighter, 29, 29, { active_weapon = 1, weapons = {weapon_class.iron_sword}, team = self.teams[2] })
-    -- unit_layer:create_unit(unit_class.axe_fighter, 26, 24, { active_weapon = 1, weapons = {weapon_class.iron_axe}, team = self.teams[2] })
-    -- unit_layer:create_unit(unit_class.lance_fighter, 27, 20, { active_weapon = 1, weapons = {weapon_class.iron_lance}, team = self.teams[2] })
-    -- unit_layer:create_unit(unit_class.bow_fighter, 22, 26, { active_weapon = 1, weapons = {weapon_class.iron_bow}, team = self.teams[2] })
+    unit_layer:create_unit(unit_class.sword_fighter, 4, 29, { active_weapon = 1, weapons = {weapon_class.iron_sword}, team = self.teams[2] })
+    unit_layer:create_unit(unit_class.axe_fighter, 1, 24, { active_weapon = 1, weapons = {weapon_class.iron_axe}, team = self.teams[2] })
+    unit_layer:create_unit(unit_class.lance_fighter, 3, 20, { active_weapon = 1, weapons = {weapon_class.iron_lance}, team = self.teams[2] })
+    unit_layer:create_unit(unit_class.bow_fighter, 2, 26, { active_weapon = 1, weapons = {weapon_class.iron_bow}, team = self.teams[2] })
 
     self.observer:add_listener("new_turn", function() self:new_turn() end)
     self.observer:add_listener("animation_ended", function() self:clean_dead_units() end)
