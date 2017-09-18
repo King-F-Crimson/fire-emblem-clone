5141bc8b20cc8d6d54dc84406c5320759d5abbb9
Pieter Douglas <pd.pieterdouglas@gmail.com>
2017-09-18 21:40:58 +1200

    Implement smooth movement animation.

--- generate_path.lua
+++ generate_path.lua
@@ -1,3 +1,5 @@
+require("utility")
+
 function generate_path(destination_tile, movement_tiles)
     local function key(x, y) return string.format("(%i, %i)", x, y) end
 
@@ -11,5 +13,8 @@ function generate_path(destination_tile, movement_tiles)
         current_tile = movement_tiles[key(current_tile.come_from.x, current_tile.come_from.y)]
     end
 
-    return path
+    -- Insert the final tile, which is the origin tile.
+    table.insert(path, current_tile)
+
+    return reverse_table(path)
 end
--- move_animation.lua
+++ move_animation.lua
@@ -10,11 +10,11 @@ function move_animation.create(args)
 
     self.path = args.path
 
-    self.length = 30
+    self.length_per_tile = 10
+
+    self.length = self.length_per_tile * (#self.path - 1)
     self.current_frame = 1
     self.complete = false
-
-    print("create run")
 
     return self
 end
@@ -31,10 +31,24 @@ end
 
 function move_animation:draw()
     local unit = self.unit
+    local x, y = self:determine_position()
 
     love.graphics.push()
         love.graphics.scale(tile_size / unit_size)
 
-        unit:draw("run", unit.tile_x * unit_size, unit.tile_y * unit_size)
+        unit:draw("run", x * unit_size, y * unit_size)
     love.graphics.pop()
+end
+
+function move_animation:determine_position()
+    local current_tile_index = math.floor((self.current_frame - 1) / self.length_per_tile) + 2
+    local current_tile = self.path[current_tile_index]
+    local previous_tile = self.path[current_tile_index - 1]
+
+    -- Determines how far the unit has moved between two tiles.
+    local accumulator = ((self.current_frame - 1) % self.length_per_tile) / self.length_per_tile
+
+    local x, y = current_tile.x * accumulator + previous_tile.x * (1 - accumulator), current_tile.y * accumulator + previous_tile.y * (1 - accumulator)
+
+    return x, y
 end
